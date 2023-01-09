01afc8073b99b771370343f65ba7eef209170923
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2023-01-09 19:57:35 +0100

    index

--- build-index.sh
+++ build-index.sh
@@ -2,5 +2,6 @@
 
 tree -H '.' -L 1 --noreport --charset utf-8 -o index.html
 
-(cd textures-cgbookcase && ./build-index.sh)
+#(cd textures-cgbookcase && ./build-index.sh)
+(cd anims && tree -H '.' -L 1 --noreport --charset utf-8 -o index.html)
 
