e2f6ab56d10d5d7bfa2ab58c8b8be877fc2d4f7f
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2023-02-21 23:26:49 +0100

    build index for skyboxes too

--- build-index.sh
+++ build-index.sh
@@ -4,4 +4,4 @@ tree -H '.' -L 1 --noreport --charset utf-8 -o index.html
 
 #(cd textures-cgbookcase && ./build-index.sh)
 (cd anims && tree -H '.' -L 1 --noreport --charset utf-8 -o index.html)
-
+(cd skyboxes && tree -H '.' -L 1 --noreport --charset utf-8 -o index.html)
